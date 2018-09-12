d42c88a1d7c1ea1e5d193b518b1d6c6a2e80b6d8
ciciiiruiz <ciciiiruiz@gmail.com>
2018-09-12 19:56:42 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,12 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+
+puts "Guest name."
+  guest_name = gets.chomp.upcase
+puts "Party name."
+  party_name = gets.chomp.upcase
+puts "Please type in the date."
+  date = gets.chomp.upcase
+puts "Please type in the time."
+  time = gets.chomp.upcase
